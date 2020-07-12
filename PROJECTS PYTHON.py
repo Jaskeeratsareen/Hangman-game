2eba4a398758a03bfc77003f1c2f6b863b215118
Jaskeeratsareen <64034739+Jaskeeratsareen@users.noreply.github.com>
2020-07-12 14:18:17 +0530

    Update PROJECTS PYTHON.py

--- PROJECTS PYTHON.py
+++ PROJECTS PYTHON.py
@@ -1,30 +1,30 @@
 # Guessing game two players prototype
-#
-# arr = []
-# output = int(input("enter the length of the character for words: "))
-# print("Enter the Alphabets: ")
-# for i in range(output):
-#     data = str(input())
-#     arr.append(data)
-#
-# n = 5
-# while n > 0:
-#     arr1 = []
-#     output1 = int(input("enter the length of the character for words: "))
-#     print("Enter the Alphabets: ")
-#     for j in range(output1):
-#         data1 = str(input())
-#         arr1.append(data1)
-#     if arr1[j] != arr[i]:
-#         print("try again")
-#         n = n - 1
-#     elif arr1[j] == arr[i]:
-#         print("YOU WON!!")
-#         break
-# if arr1[j] != arr[i]:
-#     print("GAME OVER,YOU LOST!!")
-# else:
-#     pass
+
+arr = []
+output = int(input("enter the length of the character for words: "))
+print("Enter the Alphabets: ")
+for i in range(output):
+    data = str(input())
+    arr.append(data)
+
+n = 5
+while n > 0:
+    arr1 = []
+    output1 = int(input("enter the length of the character for words: "))
+    print("Enter the Alphabets: ")
+    for j in range(output1):
+        data1 = str(input())
+        arr1.append(data1)
+    if arr1[j] != arr[i]:
+        print("try again")
+        n = n - 1
+    elif arr1[j] == arr[i]:
+        print("YOU WON!!")
+        break
+if arr1[j] != arr[i]:
+    print("GAME OVER,YOU LOST!!")
+else:
+    pass
 
 
 
